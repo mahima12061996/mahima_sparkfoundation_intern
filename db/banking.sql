acbbc68d10d8502024464d20564bd843237c15e2
mahima12061996 <64643192+mahima12061996@users.noreply.github.com>
2021-06-12 20:15:48 +0530

    Add files via upload

--- db/banking.sql
+++ db/banking.sql
@@ -1,1 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2021 at 11:58 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banking_table`
+--
+
+CREATE TABLE `banking_table` (
+  `ID` int(255) NOT NULL,
+  `NAME` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `CURRENT_BALANCE` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `banking_table`
+--
+
+INSERT INTO `banking_table` (`ID`, `NAME`, `EMAIL`, `CURRENT_BALANCE`) VALUES
+(10, 'Teddy Bear', 'teddybear@gmail.com', '600'),
+(11, 'Ajit Gupta', 'ajit@gmail.com', '0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banking_table`
+--
+ALTER TABLE `banking_table`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banking_table`
+--
+ALTER TABLE `banking_table`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
